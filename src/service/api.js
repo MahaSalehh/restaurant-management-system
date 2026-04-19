2c66e6d60c4880a5f67a9f6b0de0c2290d54b8a6
MahaSalehh <mahamsaleh279@gmail.com>
2026-04-19 21:26:59 +0200

    some updates

--- src/service/api.js
+++ src/service/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const STORAGE_URL =
-  API_BASE_URL.replace("/api", "") + "/storage/";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
 });
+
+export const STORAGE_URL = `${API_BASE_URL}/storage/`;
 
 // ==========================
 // Request Interceptor (Token + Headers handling)
